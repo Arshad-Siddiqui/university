0360fb451636cbc82bf6b1e0684a664451dba6c5
arshadsiddiqui <ferozarshad123@gmail.com>
2025-06-01 16:36:30 +0100

    Complete vector.py

--- week4/python_/vector.py
+++ week4/python_/vector.py
@@ -1,5 +1,5 @@
 import copy
-
+import math
 # Exercise 1
 
 class Vector:
@@ -49,7 +49,36 @@ class Vector:
     if not isinstance(other, Vector):
       return None
     
-    ## Note: Come back when you aren't tired
+    for i in range(self.dim()):
+      if not math.isclose(self.get(i), other.get(i)):
+        return False
+      return True
+    
+# Exercise 8
+  def __eq__(self, other):
+    if not isinstance(other, Vector):
+      return None
+    
+    for i in range(self.dim()):
+      if not math.isclose(self.get(i), other.get(i)):
+        return False
+      return True
+    
+  def __ne__(self, other):
+    if not isinstance(other, Vector):
+      return None
+    
+    for i in range(self.dim()):
+      if not math.isclose(self.get(i), other.get(i)):
+        return True
+      return False
+
+# Exercise 9:
+  def __mul__(self, number):
+    return self.scalar_product(number)
+  
+  def __iadd__(self, number):
+    return self + number
 
 vec1 = Vector([2.3, 1.0, 9.7])
 print(vec1)
@@ -57,5 +86,12 @@ print('vec1 dimention: ' + str(vec1.dim()))
 print('vec1 * 3' + str(vec1.scalar_product(3)))
 
 vec2 = Vector([7.7, 3.0, 0.3])
+vec3 = copy.copy(vec2)
 
 print(vec1 + vec2)
+
+print("check that vec2 and vec 2 are equal")
+print(vec2.equals(vec3))
+
+print("check that vec1 and vec3 are equal")
+print(vec1.equals(vec3))
