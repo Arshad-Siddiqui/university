8fb91c0d3fed192d162dd0b946eaa9db1502403e
arshadsiddiqui <ferozarshad123@gmail.com>
2025-06-02 13:34:58 +0100

    Cover linked lists

--- algorithms/week4/python_/array_stack.py
+++ algorithms/week4/python_/array_stack.py
@@ -1,5 +1,7 @@
-class ArrayStack:
-  def __init__(self):
+# Big mess come back to 4.4.3 activity
+
+class Stack:
+  def __init__(self, size=1000):
     self.array = []
 
   def push(self, value):
@@ -10,3 +12,24 @@ class ArrayStack:
   
   def top(self):
     return self.array[-1]
+  
+  def is_empty(self):
+    return len(self.array) == 0
+  
+def is_matched(expr):
+  '''Return True if all delimiters are correctly matched; False otherwise'''
+  s = Stack()
+  left = '({[' # opening delimiters
+  right = ')}]' # closing delimiters
+  for c in expr:
+    if c in left:
+      s.push(c) # push left delimiters to stack
+    elif c in right:
+      if s.is_empty():
+        return False # no matching opening delimiter
+      if right.index(c) != left.index(s.pop()):
+        return False # mismatched delimiter
+    return s.is_empty() # were all symbols matched? True/False
+  
+print(is_matched("(())}"))
+print(is_matched("()"))
