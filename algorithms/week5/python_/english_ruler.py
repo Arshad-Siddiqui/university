bd13d74e747ab6647174bbc7b23d7ece40d002bf
Arshad Siddiqui <ferozarshad123@gmail.com>
2025-06-03 21:09:57 +0100

    Refactor draw_line and draw_ruler functions for clarity and correctness

--- algorithms/week5/python_/english_ruler.py
+++ algorithms/week5/python_/english_ruler.py
@@ -3,20 +3,20 @@
 def draw_line(length, label=''):
     """Prints out a string of '-'s at specified length and marks it with an optional label"""
     line = "-" * length
-    if label:
+    if label or label == 0:
         line += ' ' + str(label)
     print(line)
 
 def draw_ruler(inches, major_length):
     for i in range(inches):
-        draw_line(major_length, str(i))
+        draw_line(major_length, i)
         draw_interval(major_length)
-    draw_line(major_length, str(inches))
-    
-def draw_interval(major_length):
-    if major_length <= 1:
+    draw_line(major_length, inches)
+
+def draw_interval(center_length):
+    if center_length == 0:
         return
-    draw_interval(major_length -1)
-    draw_line(major_length - 1)
-    draw_interval(major_length - 1)
-draw_ruler(3, 5)
+    draw_interval(center_length -1)
+    draw_line(center_length)
+    draw_interval(center_length - 1)
+draw_ruler(6, 3)
