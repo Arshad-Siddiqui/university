a9294ffe7998a2b88aecfc48bdd5a8957bf7894a
Arshad Siddiqui <ferozarshad123@gmail.com>
2025-06-03 21:29:20 +0100

    Binary search can return a boolean

---
+++ algorithms/week5/python_/binary_search.py
@@ -0,0 +1,7 @@
+# Recursive Binary Search
+
+def binary_search(data, target) -> bool:
+    """Returns true if target is contained in data"""
+    return True
+
+assert binary_search([2, 8, 12, 18], 12) == True, '12 is supposed to be present'
