a85f825ff4ea261caaee0c9408ddd2de1d9d5ccf
Arshad Siddiqui <ferozarshad123@gmail.com>
2025-06-03 21:01:43 +0100

    My english ruler works yay!

--- algorithms/week5/python_/english_ruler.py
+++ algorithms/week5/python_/english_ruler.py
@@ -1,10 +1,22 @@
-# English Ruler and Recursion
+# Incredibly messy implementation but it sorta works?
 
-def draw_ruler(length):
-  if length == 1:
-    return
+def draw_line(length, label=''):
+    """Prints out a string of '-'s at specified length and marks it with an optional label"""
+    line = "-" * length
+    if label:
+        line += ' ' + str(label)
+    print(line)
 
-  remainder = length % 4
-  print(remainder * '-')
-  draw_ruler(length - 1)
-draw_ruler(8)
+def draw_ruler(inches, major_length):
+    for i in range(inches):
+        draw_line(major_length, str(i))
+        draw_interval(major_length)
+    draw_line(major_length, str(inches))
+    
+def draw_interval(major_length):
+    if major_length <= 1:
+        return
+    draw_interval(major_length -1)
+    draw_line(major_length - 1)
+    draw_interval(major_length - 1)
+draw_ruler(3, 5)
