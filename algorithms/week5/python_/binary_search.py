5a34801e8471dc8d990afc2ad6e8129a76e9cc78
Arshad Siddiqui <ferozarshad123@gmail.com>
2025-06-03 21:50:01 +0100

    Wrote incorrect binary search

--- algorithms/week5/python_/binary_search.py
+++ algorithms/week5/python_/binary_search.py
@@ -1,7 +1,18 @@
 # Recursive Binary Search
 
-def binary_search(data, target) -> bool:
-    """Returns true if target is contained in data"""
-    return True
+def binary_search(data, target, low, high) -> bool:
+    """Returns true if target is contained in data. Low and high mark which part of the data to check"""
+    if low > high:
+        return False
 
-assert binary_search([2, 8, 12, 18], 12) == True, '12 is supposed to be present'
+    mid = int((low + high)/2)
+
+    if target == data[mid]:
+        return True
+    if data[mid] > target:
+        return binary_search(data, target, low, mid)
+    else:
+        return binary_search(data, target, mid, high)
+
+# print(binary_search([2, 8, 12, 18], 12, 0, 3))
+print(binary_search([2, 8, 12, 18], 11, 0, 3))
