0d61be5eb601652d520389819b57fe488b04f041
Arshad Siddiqui <ferozarshad123@gmail.com>
2025-06-08 13:19:38 +0100

    Exercise 2 works but needs tweaks

--- algorithms/week5/python_/exercises.py
+++ algorithms/week5/python_/exercises.py
@@ -2,13 +2,23 @@
 
 def ispalindrome(word, n=0) -> bool:
     m = n + 1
-    if n == len(word):
+    # Check half the word to save time.
+    if n >= len(word) // 2:
         return True
 
     if word[n] != word[-m]:
         return False
     return ispalindrome(word, n + 1)
 
-# I could improve the algorithm by stopping it halfway
-print(ispalindrome("racecar"))
-print(ispalindrome("racecarnt"))
+assert ispalindrome("racecar") == True
+assert ispalindrome("racecarnt") == False
+
+# Exercise 2
+
+def rec_sum(numbers, n):
+    if n == 0:
+        return numbers[0]
+    return numbers[n] + rec_sum(numbers, n-1)
+
+assert rec_sum([2, 3, 5], 2) == 10
+
