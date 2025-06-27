61370d587d77dc4169d35c777bf706edbb1b3fd4
Arshad Siddiqui <ferozarshad123@gmail.com>
2025-06-27 20:04:39 +0100

    Complete first iteration of question 1

--- algorithms/assignment/question_1.py
+++ algorithms/assignment/question_1.py
@@ -2,27 +2,70 @@ def word_encoder(word):
     if not word.isalpha():
         raise ValueError("Word must only contain alphabet characters")
     
-    first = word[0].upper()
-    rest = word[1:].lower()
-
+    # Map that converts values into corresponding numbers
     converter = {}
-
     converter.update(dict.fromkeys(["b", "f", "p", "v"], 1))
     converter.update(dict.fromkeys(["c", "g", "j", "k", "q", "s", "x", "z"], 2))
     converter.update(dict.fromkeys(["d", "t"], 3))
     converter.update(dict.fromkeys(["l"], 4))
     converter.update(dict.fromkeys(["m", "n"], 5))
     converter.update(dict.fromkeys(["r"], 6))
+    # --- Map ends here ---
 
-    drop_list = ["a", "e", "i", "o", "u", "y", "h", "w"]
+    first = word[0]
+    drop_vowels = ["a", "e", "i", "o", "u"]
+    drop_consonants = ["y", "h", "w"]
 
     converted = ""
+    low_word = word.lower()
 
-    for character in rest:
-        if character in drop_list:
-            continue
-        converted += str(converter[character])
+    # Converts all values into numbers if they map
+    for character in low_word:
+        if character in drop_vowels:
+            converted += character
+        elif character in drop_consonants:
+            # consonants aren't making it to the converted list
+            # so we can compare adjacent values.
+            pass
+        else:
+            converted += str(converter[character])
+
+    result = []
+
+    previous = None
+    for current in converted:
+        if current != previous:
+            result.append(current)
+        previous = current
     
-    return first + converted
+    converted = "".join(result)
 
-print(word_encoder("abcdefghijklmnopqrstuvwxyz"))
+    # Duplicates decimated now need to just remove vowels
+
+    for vowel in drop_vowels:
+        converted = converted.replace(vowel, "")
+
+    converted = first + converted[1:]
+
+    # Covers case where word is too short and needs trailing 0s
+    # to make up 3 digits.
+    while len(converted) < 4:
+        converted += '0'
+
+    return converted
+
+test_list = [
+    ["abcdefghijklmnopqrstuvwxyz", "checks none of the values break anything"],
+    ["ab", "check for a short sequence, should be a100"],
+    ["Rubin", "Rubin should be R150"],
+    ["Rupert", "Rupert should be R163"],
+    ["Robert", "Robert should be R163"],
+    ["Tymczak", "Tymczak should be T522"],
+    ["Pfister", "Pfister should be P263"]
+    ]
+
+for test in test_list:
+    print("-----")
+    print(word_encoder(test[0]))
+    print(test[1])
+    print("-----")
