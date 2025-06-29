5eb8216f16b2b5c6630fbebe40d8ae2c00d438ce
arshadsiddiqui <ferozarshad123@gmail.com>
2025-06-29 16:05:33 +0100

    Begin work on constructor

---
+++ algorithms/assignment/question_3.py
@@ -0,0 +1,10 @@
+# Question 3: Pathfinder
+
+class PathFinder():
+  def __init__(self, width, height, rover_x, rover_y, rover_max_gradient):
+    self.width = width
+    self.height = height
+
+    
+  
+
