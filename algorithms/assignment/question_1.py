278c032521c25312c5e54f4bbb9809fdbfa7e652
arshadsiddiqui <ferozarshad123@gmail.com>
2025-06-29 22:16:13 +0100

    Document Q1

--- algorithms/assignment/question_1.py
+++ algorithms/assignment/question_1.py
@@ -1,4 +1,16 @@
 def word_encoder(word):
+    """
+    Indexes words by sound so words that are spelt different but produce the same sound are matched.
+
+    Args:
+        word (string): A singular word with no spaces. 
+    
+    Raises:
+        ValueError: When word given contains non alphabet characters.
+
+    Returns:
+        str: String which is 4 long containing the first character in uppercase and 3 numbers.
+    """
     if not word.isalpha():
         raise ValueError("Word must only contain alphabet characters")
     
@@ -12,7 +24,8 @@ def word_encoder(word):
     converter.update(dict.fromkeys(["r"], 6))
     # --- Map ends here ---
 
-    first = word[0]
+    # Case insensitive so the first letter is always uppercase to keep output consistent.
+    first = word[0].upper()
 
     drop_vowels = ["a", "e", "i", "o", "u"]
     drop_consonants = ["y", "h", "w"]
@@ -22,14 +35,12 @@ def word_encoder(word):
         if character in drop_vowels:
             converted += character
         elif character in drop_consonants:
-            # consonants aren't making it to the converted list
-            # so we can compare adjacent values.
+            # Drop these to prevent them affecting adjacent sound groupings.
             pass
         else:
             converted += str(converter[character])
 
     result = []
-
     previous = ""
     for current in converted:
         if current != previous:
@@ -42,4 +53,5 @@ def word_encoder(word):
     for vowel in drop_vowels:
         tail = tail.replace(vowel, "")
 
+    # Ensures final output is 4 characters long by adding trailing 0s or by trimming off excess.
     return (first + tail + "000")[:4]
